1afed689f372804a70271ee3b8e4bee49dcf6e96
kartik <kartikvenkat98@gmail.com>
2017-06-02 12:17:02 +0530

    Add basic code for the board

---
+++ tictactoe.py
@@ -0,0 +1,27 @@
+print('Welcome to Tic Tac Toe !!!')
+
+print '\n'
+
+board = [' '] * 10 
+
+print('   |   |')
+print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
+print('   |   |')
+
+print('-----------')
+
+print('   |   |')
+
+print(' ' + board[4] + ' | ' + board[5] + ' | ' + board[6])
+
+print('   |   |')
+
+print('-----------')
+
+print('   |   |')
+
+print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
+
+print('   |   |')
+
+print '\n'
