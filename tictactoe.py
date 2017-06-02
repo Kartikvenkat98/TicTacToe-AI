14320da3bb3cd0d8efe1b7777dca7b365cd66b8c
kartik <kartikvenkat98@gmail.com>
2017-06-02 12:19:43 +0530

    Remove unwanted lines

--- tictactoe.py
+++ tictactoe.py
@@ -11,17 +11,13 @@ print('   |   |')
 print('-----------')
 
 print('   |   |')
-
 print(' ' + board[4] + ' | ' + board[5] + ' | ' + board[6])
-
 print('   |   |')
 
 print('-----------')
 
 print('   |   |')
-
 print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
-
 print('   |   |')
 
 print '\n'
