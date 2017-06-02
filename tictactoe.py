9f9f8b8642e34971a6ea9149f55ad32831a846a6
kartik <kartikvenkat98@gmail.com>
2017-06-02 13:33:36 +0530

    Add code to choose character and to determine who will play first

--- tictactoe.py
+++ tictactoe.py
@@ -1,3 +1,5 @@
+import random
+
 print('Welcome to Tic Tac Toe !!!')
 
 print '\n'
@@ -21,3 +23,40 @@ print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
 print('   |   |')
 
 print '\n'
+
+letter = ''
+print("Do you want to be 'X' or 'O' ??")
+
+while not (letter == 'X' or letter == 'O'):
+	
+	letter = input()
+
+	if letter == 'X':
+
+		playerLetter = 'X'
+		computerLetter = 'O'
+	
+	elif letter == 'O':
+
+		playerLetter = 'O'
+		computerLetter = 'X'
+
+	else:
+
+		print 'Please check the character entered !!'
+		print "Do you want to be 'X' or 'O' ??"
+
+print 'You are ' + playerLetter + '\n'
+print 'Computer is ' + computerLetter + '\n'
+
+toss = random.randint(0, 1)
+
+if toss == 0:
+
+	turn = 'computer'
+
+else:
+
+	turn = 'player'
+
+print 'The ' + turn + ' will go first.\n'
