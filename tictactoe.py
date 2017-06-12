1cdcd17f73b8dc2217cce466db1aa55753f9e7de
kartik <kartikvenkat98@gmail.com>
2017-06-12 15:48:28 +0530

    Add proper comments to the code

--- tictactoe.py
+++ tictactoe.py
@@ -2,6 +2,28 @@ import random
 
 def drawBoard(board):
 
+	"""
+
+	This function prints out the board
+
+	"board" is a list of 10 strings representing the board (ignore index 0)
+
+
+			|		|			
+		1	|	2	|	3	
+			|		|
+	-------------------------
+			|		|		
+		4	|	5	|	6
+			|		|
+	-------------------------
+			|		|
+		7	|	8	|	9
+			|		|
+
+
+	"""
+
 	print('   |   |')
 	print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
 	print('   |   |')
@@ -22,6 +44,8 @@ def drawBoard(board):
 
 	
 def inputLetter():
+
+	# Lets the player select which letter they want to be.
 
 	letter = ''
 	print("Do you want to be 'X' or 'O' ??")
@@ -47,6 +71,8 @@ def inputLetter():
 
 def toss():
 
+	# Randomly choose who goes first
+
 	if random.randint(0, 1) == 0:
 
 		return 'computer'
@@ -58,10 +84,14 @@ def toss():
 
 def isSpaceFree(board, move):
 
+	# Return true if the passed move is free on the passed board.
+
 	return board[move] == ' '
 
 
 def getPlayerMove(board):
+
+	# Let the player type in their move.
 
 	move = ' '
 
@@ -81,24 +111,26 @@ def makeMove(board, letter, move):
 
 def isWinner(board, letter):
 
-	return ((board[1] == letter and board[2] == letter and board[3] == letter) or
+	return ((board[1] == letter and board[2] == letter and board[3] == letter) or # across the top
 
-	(board[4] == letter and board[5] == letter and board[6] == letter) or
+	(board[4] == letter and board[5] == letter and board[6] == letter) or # across the middle
 
-	(board[7] == letter and board[8] == letter and board[9] == letter) or
+	(board[7] == letter and board[8] == letter and board[9] == letter) or # across the bottom
 
-	(board[1] == letter and board[4] == letter and board[7] == letter) or
+	(board[1] == letter and board[4] == letter and board[7] == letter) or # down the left side
 
-	(board[2] == letter and board[5] == letter and board[8] == letter) or
+	(board[2] == letter and board[5] == letter and board[8] == letter) or # down the middle
 
-	(board[3] == letter and board[6] == letter and board[9] == letter) or
+	(board[3] == letter and board[6] == letter and board[9] == letter) or # down the right side
 
-	(board[3] == letter and board[5] == letter and board[7] == letter) or
+	(board[3] == letter and board[5] == letter and board[7] == letter) or # diagonal
 
-	(board[1] == letter and board[5] == letter and board[9] == letter))
+	(board[1] == letter and board[5] == letter and board[9] == letter)) # diagonal
 
 
 def isBoardFull(board):
+
+	# Return True if every space on the board has been taken. Otherwise return False.
 
 	for i in range(1, 10):
 
@@ -111,6 +143,8 @@ def isBoardFull(board):
 
 def getDuplicate(board):
 
+	# Make a duplicate of the board list and return it the duplicate.
+
 	duplicate = []
 
 	for i in board:
@@ -122,6 +156,10 @@ def getDuplicate(board):
 
 
 def choose(board, list):
+
+	# Returns a valid move from the passed list on the passed board.
+
+	# Returns None if there is no valid move.
 
 	moves = []
 
@@ -145,6 +183,7 @@ def choose(board, list):
 
 def getComputerMove(board, computerLetter):
 
+	# Given a board and the computer's letter, determine where to move and return that move.
 
 	if computerLetter == 'X':
 
@@ -156,6 +195,8 @@ def getComputerMove(board, computerLetter):
 
 	#print playerLetter
 
+
+	# First, check if it can win in the next move
 
 	for i in range(1, 10):
 
@@ -171,6 +212,8 @@ def getComputerMove(board, computerLetter):
 				return i
 
 
+	# Check if the player could win on their next move, and block them.
+
 	for i in range(1, 10):
 
 		copy = getDuplicate(board)
@@ -185,6 +228,8 @@ def getComputerMove(board, computerLetter):
 				return i
 
 
+	# Try to take one of the corners, if they are free.
+
 	move = choose(board, [1, 3, 7, 9])
 
 	if move != None:
@@ -192,14 +237,21 @@ def getComputerMove(board, computerLetter):
 		return move
 
 
+	# Try to take the center, if it is free.
+
 	if isSpaceFree(board, 5):
 
 		return 5
+
+
+	# Move on one of the sides.
 
 	return choose(board, [2, 4, 6, 8])
 
 
 def playAgain():
+
+	# Returns True if the player wants to play again, otherwise it returns False.
 
 	print("Do you want to play again? ('yes' or 'no')")
 	return input().lower().startswith('y')
